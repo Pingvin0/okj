e72b4cc3454466b891396aa5d9e4a4f9cb1c8f4a
Bolubas Marcell <pingvinmagas@gmail.com>
2021-12-21 22:07:16 +0100

    pilotak - javitasok

--- pilotak/main.py
+++ pilotak/main.py
@@ -12,24 +12,33 @@ print(f'3. feladat: {len(adatok)}')
 print(f'4. feladat: {adatok[-1][0]}')
 
 # 19. szazad elott szuletett pilotak
-pilotak = [i for i in adatok if int(i[1][0:4]) < 1901]
-# p = padding = ures hely
-p = ' '*8
-[print(f'{p}{i[0]} ({i[1]})') for i in pilotak]
+pilotak = filter(lambda x: int(x[1][0:4]) < 1901, adatok)
+
+# ures hely
+hely = ' '*8
+for pilota in pilotak:
+    nev = pilota[0]
+    szuletes = pilota[1]
+    print(f'{hely}{nev} ({szuletes})')
 
 pilotak = sorted(list(filter(lambda x: x[-1] != -1, adatok)), key=lambda x: x[-1])
 print(f'6. feladat: {pilotak[0][2]}')
 
+# Keszitunk egy szotarat, amiben a kulcs a rajtszam,
+# es az hogy hanyszor hasznaljak az elem
 stat = {}
 for sor in adatok:
-    # A sorszam nelkuli rekordokat figyelmen kivul hagyjuk
+    # A rajtszam nelkuli rekordokat figyelmen kivul hagyjuk
     if sor[-1] == -1:
         continue
     stat[sor[-1]] = stat.get(sor[-1], 0) + 1
 
+# atalakitjuk listava
+# pelda: [(rajtszam, hasznalatszam), (rajtszam, hasznalatszam)]
 stat = stat.items()
 
+# csak akkor maradjon benne, ha a rajtszamot tobb alkalommal hasznaljak
 stat = filter(lambda x: x[1] > 1, stat)
 stat = map(lambda x: str(x[0]), stat)
-print('7. feladat: ', end='')
-print(', '.join(stat))
+
+print('7. feladat:', ', '.join(stat))
